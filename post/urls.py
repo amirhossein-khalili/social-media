daa65b5b392cfd46bc2a43351954a07994905d39
amirkhalili <amir1378khalili@gmail.com>
2024-07-07 00:13:09 +0330

    ADD delete PATH

--- post/urls.py
+++ post/urls.py
@@ -6,6 +6,7 @@ app_name = "post"
 urlpatterns = [
     path("create/", views.PostCreateView.as_view(), name="create"),
     path("edit/<int:pk>", views.PostUpdateView.as_view(), name="edit"),
+    path("delete/<int:pk>", views.PostDestroyView.as_view(), name="delete"),
     path("list/", views.PostListView.as_view(), name="list"),
     path("explore/", views.ExploreView.as_view(), name="explore"),
 ]
