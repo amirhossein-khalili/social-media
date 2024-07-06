4e4f03c23ce3f31985f6261f30ca3c70b3e1cf58
amirkhalili <amir1378khalili@gmail.com>
2024-07-07 00:12:02 +0330

    UPDATE ERROR MESSAGES

--- post/error_messages.py
+++ post/error_messages.py
@@ -1,4 +1,5 @@
 POST_NOT_FOUND = "پست مورد نظر پیدا نشد."
-UNAUTHORIZED_UPDATE = "شما مجاز به بروزرسانی این پست نیستید."
+INACCESSIBILITY_UPDATE_ERROR = "شما مجاز به بروزرسانی این پست نیستید."
+INACCESSIBILITY_DELETE_ERROR = "شما مجاز به حذف این پست نیستید"
 SERVER_ERROR = "خطایی رخ داده است لطفا در بازه زمانی دیگری مجدد تلاش کنید "
 VALIDATION_ERROR = "خطای اعتبارسنجی داده‌ها."
