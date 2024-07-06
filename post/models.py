19ddfa8a5c5eb04227df14828175fa09b8232631
amirkhalili <amir1378khalili@gmail.com>
2024-07-07 00:12:50 +0330

    ADD SLUG FIELD TO POST MODEL

--- post/models.py
+++ post/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
+    slug = models.SlugField(max_length=20, default=None)
     image = models.ImageField(upload_to="post_images/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
