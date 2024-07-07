7e75cb8b66a66b647a358bded6a748560a73f67f
amirkhalili <amir1378khalili@gmail.com>
2024-07-07 14:12:03 +0330

    ADD get absolute

--- post/models.py
+++ post/models.py
@@ -12,3 +12,6 @@ class Post(models.Model):
 
     def __str__(self):
         return f"Post by {self.author.username} at {self.created_at}"
+
+    def get_absolute_url(self):
+        return reverse("post:detail", kwargs={"pk": self.pk})
