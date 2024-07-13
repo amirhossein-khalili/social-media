9b7e4b9aebdda304fb3dcfdb1096a5bff7d03f28
amirkhalili <amir1378khalili@gmail.com>
2024-07-13 23:33:13 +0330

    ADD PostAdmin

--- post/admin.py
+++ post/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Comment, Post
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("slug", "author", "created", "updated")
-    search_fields = ("author__username", "content")
+    list_display = ("slug", "user", "created", "updated")
+    search_fields = ("user__username", "content")
     list_filter = ("created", "updated")
-    prepopulated_fields = {"slug": ("author", "content")}
-    raw_id_fields = ("author",)
+    prepopulated_fields = {"slug": ("user", "content")}
+    raw_id_fields = ("user",)
+
+
+@admin.register(Comment)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("user", "post", "created", "is_reply")
+    raw_id_fields = ("user", "post", "reply")
