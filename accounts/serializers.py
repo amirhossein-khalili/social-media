ad174a068855017de504a23b6c6217274cf70d23
amirkhalili <amir1378khalili@gmail.com>
2024-07-13 23:32:52 +0330

    REMOVE UserRegisterSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -10,43 +10,6 @@ from rest_framework_simplejwt.settings import api_settings
 from .models import User
 
 
-class UserRegisterSerializer(serializers.ModelSerializer):
-    repeat_password = serializers.CharField(required=True, write_only=True)
-
-    class Meta:
-        model = User
-        fields = ("username", "password", "email", "repeat_password")
-        extra_kwargs = {
-            "password": {"write_only": True},
-        }
-
-    def create(self, validated_data):
-        validated_data.pop("repeat_password")
-        user = User.objects.create_user(**validated_data)
-        return user
-
-    def validate_username(self, value):
-        if value.lower() == "admin":
-            raise serializers.ValidationError("Username can't be 'admin'!!")
-        if User.objects.filter(username=value).exists():
-            raise serializers.ValidationError("This username is already taken!!")
-        return value
-
-    def validate_email(self, value):
-        if "admin" in value:
-            raise serializers.ValidationError("Email can't contain 'admin' !!")
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("This email is already taken!!")
-        return value
-
-    def validate(self, data):
-        if data["password"] != data["repeat_password"]:
-            raise serializers.ValidationError(
-                "Password and repeat password must match!!"
-            )
-        return data
-
-
 class SignupStepOneSerializer(serializers.Serializer):
     email = serializers.EmailField()
     username = serializers.CharField(max_length=150)
