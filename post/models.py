95dcad74cae49de1d29d77c56f609e1359aa8d82
amirkhalili <amir1378khalili@gmail.com>
2024-07-13 23:33:24 +0330

    ADD comment model

--- post/models.py
+++ post/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Post(models.Model):
-    author = models.ForeignKey(
+    user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
     )
     content = models.TextField()
@@ -13,10 +13,32 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Post by {self.author.username} at {self.created_at}"
+        return f"Post by {self.user.username} at {self.created}"
 
     def get_absolute_url(self):
         return reverse("post:detail", kwargs={"pk": self.pk})
 
     class Meta:
         ordering = ["content"]
+
+ 
+class Comment(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="usercomments"
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="postcomments"
+    )
+    reply = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        related_name="rcomments",
+        blank=True,
+        null=True,
+    )
+    is_reply = models.BooleanField(default=False)
+    body = models.TextField(max_length=400)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post.id}"
