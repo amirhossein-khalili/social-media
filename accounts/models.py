2b478590d87f9945ea60112d19062455a401118a
amirkhalili <amir1378khalili@gmail.com>
2024-07-13 16:44:06 +0330

    ADD new fields  to user model

--- accounts/models.py
+++ accounts/models.py
@@ -5,3 +5,15 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
+
+    ##################### profile part #####################
+    bio = models.CharField(max_length=200)
+    followers_count = models.IntegerField(default=0)
+    followings_count = models.IntegerField(default=0)
+    posts_count = models.IntegerField(default=0)
+    description = models.CharField(max_length=200)
+    is_private = models.BooleanField(default=False)
+    activate = models.BooleanField(default=True)
+    is_reported = models.BooleanField(default=False)
+    #########################################################
