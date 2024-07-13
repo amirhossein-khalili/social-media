69693de4b043d1c2a122b49b632e35155cec644b
amirkhalili <amir1378khalili@gmail.com>
2024-07-13 21:36:02 +0330

    ADD Relation model

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -7,13 +7,35 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
 
-    ##################### profile part #####################
-    bio = models.CharField(max_length=200)
+    bio = models.CharField(max_length=200, blank=True)
     followers_count = models.IntegerField(default=0)
     followings_count = models.IntegerField(default=0)
     posts_count = models.IntegerField(default=0)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True)
     is_private = models.BooleanField(default=False)
     activate = models.BooleanField(default=True)
     is_reported = models.BooleanField(default=False)
-    #########################################################
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
+
+
+class Relation(models.Model):
+    from_user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="following_relations",
+    )
+
+    to_user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="follower_relations",
+    )
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "relation"
+        verbose_name_plural = "relations"
