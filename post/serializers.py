32645e437666a86ab25341107d1b25a142c265e9
amirkhalili <amir1378khalili@gmail.com>
2024-07-13 23:33:49 +0330

    ADD CommentSerializer , PostDetailSerializer

--- post/serializers.py
+++ post/serializers.py
@@ -1,28 +1,12 @@
 from rest_framework import serializers
 
-from .models import Post
-
-# class PostSerializer(serializers.ModelSerializer):
-#     author = serializers.SerializerMethodField()
-
-#     class Meta:
-#         model = Post
-#         fields = ["id", "content", "image", "created_at", "updated_at", "author"]
-#         read_only_fields = ["created_at", "updated_at", "author"]
+from .models import Comment, Post
 
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = [
-            "id",
-            "author",
-            "content",
-            "slug",
-            "image",
-            "created_at",
-            "updated_at",
-        ]
+        fields = ["id", "user", "content", "slug", "image", "created", "updated"]
 
     def validate_content(self, value):
         if len(value) < 10:
@@ -31,9 +15,57 @@ class PostSerializer(serializers.ModelSerializer):
             )
         return value
 
-    def get_author(self, obj):
-        return {"username": obj.author.username, "email": obj.author.email}
-
     def get_url(self, obj):
         request = self.context.get("request")
         return request.build_absolute_uri(obj.get_absolute_url())
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = [
+            "id",
+            "user",
+            "body",
+            "created",
+            "is_reply",
+            "reply",
+        ]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["user"] = {
+            "username": instance.user.username,
+            "email": instance.user.email,
+        }
+        return representation
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    postcomments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = [
+            "id",
+            "user",
+            "content",
+            "slug",
+            "image",
+            "created",
+            "updated",
+            "postcomments",
+        ]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["user"] = {
+            "username": instance.user.username,
+            "email": instance.user.email,
+        }
+
+        representation["user"] = {
+            "username": instance.user.username,
+            "email": instance.user.email,
+        }
+        return representation
