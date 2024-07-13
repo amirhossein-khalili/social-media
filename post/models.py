a9fa873a39caddf10b4e12a776c6d58cf855aafd
amirkhalili <amir1378khalili@gmail.com>
2024-07-13 21:51:05 +0330

    update Post model

--- post/models.py
+++ post/models.py
@@ -9,8 +9,8 @@ class Post(models.Model):
     content = models.TextField()
     slug = models.SlugField(max_length=20, default=None)
     image = models.ImageField(upload_to="post_images/", blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Post by {self.author.username} at {self.created_at}"
